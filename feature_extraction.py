9937acb3ac22b956d7f607f6fef40a3cb6192010
gzanardini <g.zanardini@student.tudelft.nl>
2025-05-27 11:34:32 +0200

    Finished statistical analysis, finalized single feature trainings fro TUH

--- feature_extraction.py
+++ feature_extraction.py
@@ -28,7 +28,7 @@ combiner_names = ['mean', 'median', 'std', 'skewness', 'kurtosis']
 
 montages = ['CAR', 'Cz', 'BipolarDB', 'Laplacian']
 
-segment_lengths = [1, 2, 5, 10]
+segment_lengths = [20,60]
 
 # for montage in montages:
 #     for segment_length in segment_lengths:
@@ -43,49 +43,49 @@ segment_lengths = [1, 2, 5, 10]
 #         spectral_features = np.array(spectral_features)
 #         print(f'Finished processing montage: {montage}, segment length: {segment_length}')
 #         # Save the features
-#         np.save(os.path.join(feature_path, f'spectral_{montage}_{segment_length}.npy'), spectral_features)
+#         np.save(os.path.join(feature_path, f'spectral_{montage}_{segment_length}s.npy'), spectral_features)
 
-# for montage in montages:
-#     for segment_lenght in segment_lengths:
-#         print(f"Starting cwt - {montage} {segment_lenght}s")
-#         cwt_features = []     
-#         for index,sample in enumerate(stim_samples):
-#             print(f'Processing sample {index+1}/{len(stim_samples)}')
-#             data= sample.get_data()
-#             cwt_features.append(yf.run_cwt_seg(data, Fs=FS, MONTAGE=montage,WAVELET_TYPE='morl' ,sec=segment_lenght))
-#         cwt_features = np.array(cwt_features)
-#         print(f'Finished processing montage: {montage}, segment length: {segment_lenght}')
-#         # Save the features
-#         np.save(os.path.join(feature_path, f'cwt_{montage}_{segment_lenght}.npy'), cwt_features)
+for montage in montages:
+    for segment_lenght in segment_lengths:
+        print(f"Starting cwt - {montage} {segment_lenght}s")
+        cwt_features = []     
+        for index,sample in enumerate(stim_samples):
+            print(f'Processing sample {index+1}/{len(stim_samples)}')
+            data= sample.get_data()
+            cwt_features.append(yf.run_cwt_seg(data, Fs=FS, MONTAGE=montage,WAVELET_TYPE='morl' ,sec=segment_lenght))
+        cwt_features = np.array(cwt_features)
+        print(f'Finished processing montage: {montage}, segment length: {segment_lenght}')
+        # Save the features
+        np.save(os.path.join(feature_path, f'cwt_{montage}_{segment_lenght}s.npy'), cwt_features)
 
-# for montage in montages:
-#     for segment_lenght in segment_lengths:
-#         print(f"Starting dwt - {montage} {segment_lenght}s")
-#         dwt_features = []
-#         for index,sample in enumerate(stim_samples):
-#             print(f'Processing sample {index+1}/{len(stim_samples)}')
-#             data= sample.get_data()
-#             dwt_features.append(yf.run_dwt_seg(data, Fs=FS, MONTAGE=montage, WAVELET='db4', sec=segment_lenght))
-#         dwt_features = np.array(dwt_features)
-#         print(f'Finished processing montage: {montage}, segment length: {segment_lenght}')
-#         # Save the features
-#         np.save(os.path.join(feature_path, f'dwt_{montage}_{segment_lenght}.npy'), dwt_features)
+for montage in montages:
+    for segment_lenght in segment_lengths:
+        print(f"Starting dwt - {montage} {segment_lenght}s")
+        dwt_features = []
+        for index,sample in enumerate(stim_samples):
+            print(f'Processing sample {index+1}/{len(stim_samples)}')
+            data= sample.get_data()
+            dwt_features.append(yf.run_dwt_seg(data, Fs=FS, MONTAGE=montage, WAVELET='db4', sec=segment_lenght))
+        dwt_features = np.array(dwt_features)
+        print(f'Finished processing montage: {montage}, segment length: {segment_lenght}')
+        # Save the features
+        np.save(os.path.join(feature_path, f'dwt_{montage}_{segment_lenght}s.npy'), dwt_features)
 
-# ## MST 
+## MST 
 
-# for montage in montages:
-#     for segment_lenght in segment_lengths:
-#         print(f"Starting mst - {montage} {segment_lenght}s")
-#         mst_features = []
-#         for index,sample in enumerate(stim_samples):
-#             print(f'Processing sample {index+1}/{len(stim_samples)}')
-#             data= sample.get_data()
-#             mst_features.append(yf.run_mST_seg(data, Fs=FS, MONTAGE=montage, epoch_width=segment_lenght))
+for montage in montages:
+    for segment_lenght in segment_lengths:
+        print(f"Starting mst - {montage} {segment_lenght}s")
+        mst_features = []
+        for index,sample in enumerate(stim_samples):
+            print(f'Processing sample {index+1}/{len(stim_samples)}')
+            data= sample.get_data()
+            mst_features.append(yf.run_mST_seg(data, Fs=FS, MONTAGE=montage, epoch_width=segment_lenght))
         
-#         mst_features = np.array(mst_features)
-#         print(f'Finished {montage} {segment_lenght}s')
-#         print(f"Shape: {mst_features.shape}")
-#         np.save(os.path.join(feature_path, f"mst_{montage}_{segment_lenght}s.npy"), mst_features)
+        mst_features = np.array(mst_features)
+        print(f'Finished {montage} {segment_lenght}s')
+        print(f"Shape: {mst_features.shape}")
+        np.save(os.path.join(feature_path, f"mst_{montage}_{segment_lenght}s.npy"), mst_features)
         
 
 ## SST
@@ -145,7 +145,8 @@ for montage in montages:
         for index,sample in enumerate(stim_samples):
             print(f'Processing sample {index+1}/{len(stim_samples)}')
             data= sample.get_data()
-            gplv_features.append(yf.run_gplv_seg(data, Fs=FS, MONTAGE=montage, sec=segment_lenght))
+            with HiddenPrints():
+                gplv_features.append(yf.run_gplv_seg(data, Fs=FS, MONTAGE=montage, sec=segment_lenght))
 
         gplv_features = np.array(gplv_features)
         print(f'Finished {montage} {segment_lenght}s')
@@ -176,7 +177,8 @@ for montage in montages:
         for index,sample in enumerate(stim_samples):
             print(f'Processing sample {index+1}/{len(stim_samples)}')
             data= sample.get_data()
-            gcc_features.append(yf.run_gcc_seg(data, Fs=FS, MONTAGE=montage, sec=segment_lenght))
+            with HiddenPrints():
+                gcc_features.append(yf.run_gcc_seg(data, Fs=FS, MONTAGE=montage, sec=segment_lenght))
         
         gcc_features = np.array(gcc_features)
         print(f'Finished {montage} {segment_lenght}s')
---
+++ tuh/tuh_singleset_whole.py
@@ -0,0 +1,171 @@
+from pytest import skip
+import wandb.plot
+from xgboost import XGBClassifier
+import numpy as np 
+import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, roc_auc_score, roc_curve, precision_recall_curve, balanced_accuracy_score
+import itertools 
+import secrets
+from scipy.stats import mannwhitneyu
+import os
+from sklearn.model_selection import train_test_split
+#warnings.simplefilter(action='ignore', category=FutureWarning)
+import cupy as cp
+from cupy.cuda import Device
+
+wandb.login(key='96e9a92e52e807ed253b3872afd1de1bafc3640a')
+
+N_RUNS=3
+N_CUDA=0
+SPLIT_RATIO=0.3
+PROJECT_NAME='tuh_LOSO_single_set_whole'
+FEAT_FOLDER='/space/gzanardini/tuh_features_whole/'
+
+Device(N_CUDA).use()
+
+def calculate_bac(labels, scores, sens_thresh):
+    fpr, tpr, thresholds = roc_curve(labels, scores)
+    valid_idxs = np.where(tpr >= sens_thresh)[0]
+    if len(valid_idxs) == 0:
+        # If no TPR >= sens_thresh, use the last threshold by default
+        threshold_sensitivity = thresholds[-1] if len(thresholds) > 0 else 0.5
+    else:
+        threshold_sensitivity = thresholds[valid_idxs[0]]
+    adjusted_predictions = (scores >= threshold_sensitivity).astype(int)
+    tn, fp, fn, tp = confusion_matrix(labels, adjusted_predictions, labels=[0 ,1] ).ravel()
+    sensitivity = tp / (tp + fn) if (tp + fn) != 0 else 0
+    specificity = tn / (tn + fp) if (tn + fp) != 0 else 0
+    bac = ((sensitivity + specificity) / 2)
+    return bac, fpr, tpr, thresholds
+
+def handle_complex_numbers(features):
+    if isinstance(features, pd.DataFrame):
+        for column in features.columns:
+            if np.iscomplexobj(features[column]):
+                # Convert to real part or magnitude
+                features[column] = features[column].apply(np.abs)  # or .apply(np.real)
+            # Replace inf and -inf with NaN
+            features[column].replace([np.inf, -np.inf], np.nan, inplace=True)
+    elif isinstance(features, np.ndarray):
+        if np.iscomplexobj(features):
+            # Convert to real part or magnitude
+            features = np.abs(features)  # or np.real(features)
+        # Replace inf and -inf with NaN
+        features[~np.isfinite(features)] = np.nan
+    return features
+
+montages=['CAR', 'Cz', 'BipolarDB','Laplacian']
+segment_lengths=[1, 2, 5,10,20,60]
+feature_names = ['cc','cwt', 'dwt', 'gcc', 'gplv','plv','mst','sst','spectral','utm']
+
+description=pd.read_csv(f'{FEAT_FOLDER}/description.csv')
+labels=description['epilepsy'].to_numpy()
+subjects=description['subject'].to_numpy()
+unique_subjects=np.unique(description['subject'])
+
+subject_labels = []
+for subj in unique_subjects:
+    lbl = labels[subjects == subj][0]
+    subject_labels.append([subj, lbl])
+subject_labels = np.array(subject_labels)
+
+for montage, feature_name, segment_length in itertools.product(montages, feature_names, segment_lengths):
+
+    features=np.load(f'{FEAT_FOLDER}{feature_name}_{montage}_s{segment_length}s.npy')
+    print(f'Loading {feature_name} features for montage {montage} and segment length {segment_length}')
+    
+    print(f'Features shape: {features.shape}')
+
+
+    if len(features.shape)>2:
+        features=features.reshape(features.shape[0], -1)
+    features=handle_complex_numbers(features)
+
+    print(f'Features shape: {features.shape}')
+
+    for run_n in range(N_RUNS):   
+        print(f'Run {run_n} - {montage} - {feature_name} - {segment_length}s')
+    
+        wandb.init(project=PROJECT_NAME, name=f'{feature_name}_{montage}_{segment_length}s_run_{run_n}', reinit=True)
+
+        seed=secrets.randbelow(5000)
+        np.random.seed(seed)
+        cp.random.seed(seed)
+        wandb.config.seed=seed
+        wandb.config.montage=montage
+        wandb.config.feature_name=feature_name
+        wandb.config.segment_length=segment_length
+        
+        y_preds=[]
+        y_scores=[]
+        y_tests=[]
+
+        ctr=0
+
+        for ss, subject in enumerate(unique_subjects):
+            print(f'Iteration {ss+1} - Subject: {subject}')
+            test_idxs = np.where(description['subject'] == subject)
+
+            other_subjects = [subj_oth for subj_oth in unique_subjects if subj_oth != subject]
+            other_subjects_labels = np.array([[subj_oth, labels[subjects == subj_oth][0]] for subj_oth in other_subjects])
+            train_subjects = np.array(other_subjects)
+        
+            train_idxs = np.where(np.isin(description['subject'], train_subjects))[0]
+            
+            y_train=labels[train_idxs].astype(int)
+            y_test=labels[test_idxs].astype(int)
+
+            ratio=(len(y_train)-sum(y_train))/sum(y_train) 
+
+            model=XGBClassifier(n_estimators=100, max_depth=7, device=f'cuda:{N_CUDA}', seed=seed, subsample=0.8, scale_pos_weight=ratio, n_jobs=4, gamma=0.1, learning_rate=0.05)
+            model.fit(cp.array(features[train_idxs]), cp.array(labels[train_idxs]))
+
+            print('Training data shape:', features[train_idxs].shape)
+            print('Test data shape:', features[test_idxs].shape)
+
+            y_pred=model.predict(cp.array(features[test_idxs]))
+            y_score=model.predict_proba(cp.array(features[test_idxs]))[:,1]
+
+            y_preds.extend(y_pred)
+            y_scores.extend(y_score)
+            y_tests.extend(y_test)
+
+        y_preds=np.array(y_preds).flatten()
+        y_scores=np.array(y_scores).flatten()
+        y_tests=np.array(y_tests).flatten()
+
+        print(f'Y_preds shape: {y_preds.shape}')
+        print(f'Y_scores shape: {y_scores.shape}')
+        print(f'Y_tests shape: {y_tests.shape}')
+               
+        bac=balanced_accuracy_score(y_tests, y_preds)
+        bac80, fpr, tpr, thresholds = calculate_bac(y_tests, y_scores, 0.8)
+        auc=roc_auc_score(y_tests, y_scores)
+        recall=recall_score(y_tests, y_preds)
+        precision=precision_score(y_tests, y_preds)
+        f1=f1_score(y_tests, y_preds)
+        accuracy=accuracy_score(y_tests, y_preds)
+
+        c_m = wandb.plot.confusion_matrix(y_true=y_tests, preds=y_preds, class_names=['healthy', 'epileptic'])
+
+        data_roc = [[f, t] for (f, t) in zip(fpr, tpr)]
+        table_roc = wandb.Table(data=data_roc, columns=["fpr", "tpr"])
+        roc_line=wandb.plot.line(table_roc, "fpr", "tpr", title="ROC Curve")
+
+        p , r , t = precision_recall_curve(y_tests, y_scores)
+        data_pr = [[f, t] for (f, t) in zip(p, r)]
+        table_pr = wandb.Table(data=data_pr, columns=["precision", "recall"])
+        pr_line=wandb.plot.line(table_pr, "precision", "recall", title="Precision-Recall Curve")
+
+        wandb.log({'BAC': bac,
+                     'BAC80': bac80,
+                     'AUC': auc,
+                     'Recall': recall,
+                     'Precision': precision,
+                     'F1': f1,
+                     'Confusion Matrix': c_m,
+                     'ROC Curve': roc_line,
+                     'Precision-Recall Curve': pr_line,
+                     'Accuracy': accuracy})
+        
+        wandb.finish() 
